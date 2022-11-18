7a3c645871ff203b162ab97cea1fb5321495aa9e
Nicolas Vicencio <nicolas.vic.or@protonmail.com>
2022-11-17 21:53:42 -0300

    new commit in dev branch

--- app/src/main/java/com/example/mothman/MainActivity.java
+++ app/src/main/java/com/example/mothman/MainActivity.java
@@ -5,12 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private String hola = "hola";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
 
     }
 }
