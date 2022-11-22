fb5f70f9f6dceb3c57778571970b528c38a09a16
Nicolas Vicencio <nicolas.vic.or@protonmail.com>
2022-11-22 20:10:33 -0300

    Creating list of sensors and appling a onClick listener

--- app/src/main/java/com/example/mothman/ListData.java
+++ app/src/main/java/com/example/mothman/ListData.java
@@ -6,10 +6,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.mothman.Controllers.SensorController;
+import com.example.mothman.Models.Sensor;
 
 import java.util.ArrayList;
 
@@ -23,10 +27,46 @@ public class ListData extends AppCompatActivity {
         setContentView(R.layout.activity_list_data);
 
         SensorController.fillSensorList();
+        AdapterSensor adapter = new AdapterSensor(this);
+
         sensorList = (ListView) findViewById(R.id.listViewData);
+        sensorList.setAdapter(adapter);
+
+        sensorList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(ListData.this, "Sensor: "+SensorController.findAll().get(i).getName(), Toast.LENGTH_SHORT);
+            }
+        });
+
+
     }
 
-    
+    class AdapterSensor extends ArrayAdapter<Sensor>{
+        AppCompatActivity appCompatActivity;
+
+        public AdapterSensor(AppCompatActivity context){
+            super(context, R.layout.sensor_layout, SensorController.findAll());
+            appCompatActivity = context;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = appCompatActivity.getLayoutInflater();
+            View item = inflater.inflate(R.layout.sensor_layout, null);
+
+            TextView tvSensorName = item.findViewById(R.id.tvSensorName);
+            tvSensorName.setText(SensorController.findAll().get(position).getName());
+
+            TextView tvSensorData = item.findViewById(R.id.tvSensorData);
+            tvSensorData.setText(SensorController.findAll().get(position).getOutput());
+
+            return item;
+        }
+
+    }
+
+
 
 }
 
