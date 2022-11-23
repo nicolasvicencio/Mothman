8d482f5ef46ec2209634a2b838275f3eafe64157
Nicolas Vicencio <nicolas.vic.or@protonmail.com>
2022-11-23 18:27:14 -0300

    Adding id attribute to user Class

--- app/src/main/java/com/example/mothman/Models/User.java
+++ app/src/main/java/com/example/mothman/Models/User.java
@@ -1,15 +1,19 @@
 package com.example.mothman.Models;
 
 public class User {
+    private String id;
     private String username;
     private String password;
 
 
-    public User(String username, String password) {
+    public User(String id, String username, String password) {
+        this.id = id;
         this.username = username;
         this.password = password;
     }
 
+    public String getId() {return id;}
+    public void setId(String id) {this.id = id;}
     public String getUsername(){
         return this.username;
     }
